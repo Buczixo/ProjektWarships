bbc016be5dc781f1baa354f59390fe2b136f74d6
Maciej Buczak <97064870+Buczixo@users.noreply.github.com>
2024-01-02 15:21:00 +0100

    Firebase AutoUpdate Works Flawless (kinda)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -346,10 +345,4 @@ enum class CellState {
     EmptyHit,
     Ship,
     ShipHit
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewWarshipsGame() {
-    WarshipsGame()
 }
